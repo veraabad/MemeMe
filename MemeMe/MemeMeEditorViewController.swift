b3ec1c6ff571a065121991791cfacfda3f3bb126
Abad-Vera <veraabad@icloud.com>
2018-11-14 13:05:15 -0800

    Added UIImagePicker functionality

--- MemeMe/MemeMeEditorViewController.swift
+++ MemeMe/MemeMeEditorViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class MemeMeEditorViewController: UIViewController {
+class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var sharingButton: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
+    @IBAction func cameraAction(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
+    }
 
+    @IBAction func albumAction(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+
+    @IBAction func sharingAction(_ sender: Any) {
+    }
+
+    @IBAction func cancelAction(_ sender: Any) {
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let img = info[.originalImage] as? UIImage {
+            print("AV :: mediaType selected")
+            memeImageView.image = img
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
