3983017e3d8c0e44cf3beb57554ebfbf8d605968
Abad-Vera <veraabad@icloud.com>
2018-11-15 23:18:10 -0800

    Added Meme struct

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,24 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Abad Vera on 11/15/18.
+//  Copyright © 2018 Abad Vera. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText:String
+    let bottomText:String
+    let originalImage:UIImage
+    let memedImage:UIImage
+
+    init(topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage) {
+        self.topText = topText
+        self.bottomText = bottomText
+        self.originalImage = originalImage
+        self.memedImage = memedImage
+    }
+}
