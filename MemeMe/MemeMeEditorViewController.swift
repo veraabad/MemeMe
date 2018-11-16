c7921bebc277ba46433f322b76aa14ef1923d83b
Abad-Vera <veraabad@icloud.com>
2018-11-15 22:50:32 -0800

    Added Keyboard methods

--- MemeMe/MemeMeEditorViewController.swift
+++ MemeMe/MemeMeEditorViewController.swift
@@ -15,6 +15,7 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
     @IBOutlet weak var memeImageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var sharingButton: UIBarButtonItem!
+    weak var activeTextField: UITextField!
 
     let memeTextAttributes:[NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
@@ -27,19 +28,11 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
         super.viewWillAppear(animated)
 
         initialSetup()
+        subscribeToKeyboardNotifications()
     }
 
-    func initialSetup() {
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-        setupTextField(topTextField, withString: "TOP")
-        setupTextField(bottomTextField, withString: "BOTTOM")
-    }
-
-    func setupTextField(_ textField: UITextField, withString str: String) {
-        textField.defaultTextAttributes = memeTextAttributes
-        textField.textAlignment = .center
-        textField.delegate = self
-        textField.text = str
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
     }
 
     @IBAction func cameraAction(_ sender: Any) {
@@ -62,11 +55,61 @@ class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDeleg
     @IBAction func cancelAction(_ sender: Any) {
     }
 
+    // MARK: MemeMeEditorViewController helper methods
+    func initialSetup() {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        setupTextField(topTextField, withString: "TOP")
+        setupTextField(bottomTextField, withString: "BOTTOM")
+    }
+
+    func setupTextField(_ textField: UITextField, withString str: String) {
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = .center
+        textField.delegate = self
+        textField.text = str
+    }
+
+    // MARK: Keyboard methods
+    @objc func keyboardDidShow(_ notification: Notification) {
+        var viewFrame = view.frame
+        viewFrame.size.height -= getKeyboardHeight(notification)
+
+        if !viewFrame.contains(activeTextField.frame.origin) {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+
+    @objc func keyboardDidHide(_ notification: Notification) {
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
+    }
+
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+
+        return keyboardSize.cgRectValue.height
+    }
+
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide(_:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+
     // MARK: UITextFieldDelegate methods
     func textFieldDidBeginEditing(_ textField: UITextField) {
         if textField.text == "TOP" || textField.text == "BOTTOM" {
             textField.text = ""
         }
+        activeTextField = textField
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
