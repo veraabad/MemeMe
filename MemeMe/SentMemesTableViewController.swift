40854dc6c4be4e4ab2e9409e6e26c59b46efeb42
Abad-Vera <veraabad@icloud.com>
2018-12-03 08:21:06 -0800

    Initial UI setup for MemeMe 2.0

---
+++ MemeMe/SentMemesTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SentMemesTableViewController.swift
+//  MemeMe
+//
+//  Created by Abad Vera on 12/3/18.
+//  Copyright © 2018 Abad Vera. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
